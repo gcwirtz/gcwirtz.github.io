4b27a1994d39ae6bea02b1500016937cea570d13
Curtis Wirtz <supcurtis@gmail.com>
2025-01-28 02:46:28 -0800

    feat: add animation driver for viewport entry

--- assets/js/dist/script.js
+++ assets/js/dist/script.js
@@ -1,5 +1,34 @@
 "use strict";
+// Scroll triggered animations: simple intersection observer
+var animationDriver = function () {
+    // console.log('animation driver ran');
+    var items = document.querySelectorAll(".step");
+    var callback = function (entries, observer) {
+        // console.log('callback ran');
+        entries.forEach(function (entry) {
+            if (entry.isIntersecting && entry.target.classList.contains("pre-animated")) {
+                // console.log(entry);
+                observer.unobserve(entry.target);
+                // trigger animation
+                entry.target.classList.remove("pre-animated");
+            }
+        });
+    };
+    var animationObserver = new IntersectionObserver(callback, {
+        rootMargin: "0px 0px -20% 0px",
+        threshold: 0
+    });
+    var observeItems = function (items) {
+        items.forEach(function (item) {
+            item.classList.add("pre-animated");
+            animationObserver.observe(item);
+        });
+    };
+    observeItems(items);
+};
 document.addEventListener("DOMContentLoaded", function () {
+    // trigger scroll entry animations
+    animationDriver();
     console.log("js hooked up");
 });
 //# sourceMappingURL=script.js.map
--- assets/js/script.ts
+++ assets/js/script.ts
@@ -1,3 +1,39 @@
+// Scroll triggered animations: simple intersection observer
+const animationDriver = (): void => {
+  // console.log('animation driver ran');
+  const items = document.querySelectorAll(".step");
+
+  const callback = (entries: IntersectionObserverEntry[], observer: IntersectionObserver): void => {
+    // console.log('callback ran');
+    entries.forEach((entry) => {
+      if (entry.isIntersecting && entry.target.classList.contains("pre-animated")) {
+        // console.log(entry);
+
+        observer.unobserve(entry.target);
+
+        // trigger animation
+        entry.target.classList.remove("pre-animated");
+      }
+    });
+  };
+
+  const animationObserver = new IntersectionObserver(callback, {
+    rootMargin: "0px 0px -20% 0px",
+    threshold: 0
+  });
+
+  const observeItems = (items: NodeListOf<Element>): void => {
+    items.forEach((item) => {
+      item.classList.add("pre-animated");
+      animationObserver.observe(item);
+    });
+  };
+  observeItems(items);
+};
+
 document.addEventListener("DOMContentLoaded", () => {
+  // trigger scroll entry animations
+  animationDriver();
+
   console.log("js hooked up");
 });
