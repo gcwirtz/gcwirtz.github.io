8e080ca53e2545afa6af67f6345425f2b48e2060
Curtis Wirtz <supcurtis@gmail.com>
2025-05-25 21:42:19 -0700

    MANUAL BUILD PUSH

--- assets/js/dist/script.js
+++ assets/js/dist/script.js
@@ -1,12 +1,21 @@
-"use strict";
-// Scroll triggered animations: simple intersection observer
-var animationDriver = function () {
+/**
+ * Add Alpine.js to project
+ *  */
+import Alpine from "alpinejs";
+// Start Alpine
+Alpine.start();
+// console.log("Alpine.js is now running!");
+/**
+ * Scroll triggered animations: simple intersection observer
+ *  */
+const animationDriver = () => {
     // console.log('animation driver ran');
-    var items = document.querySelectorAll(".step");
-    var callback = function (entries, observer) {
+    const items = document.querySelectorAll(".step");
+    const callback = (entries, observer) => {
         // console.log('callback ran');
-        entries.forEach(function (entry) {
-            if (entry.isIntersecting && entry.target.classList.contains("pre-animated")) {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting &&
+                entry.target.classList.contains("pre-animated")) {
                 // console.log(entry);
                 observer.unobserve(entry.target);
                 // trigger animation
@@ -14,32 +23,32 @@ var animationDriver = function () {
             }
         });
     };
-    var animationObserver = new IntersectionObserver(callback, {
+    const animationObserver = new IntersectionObserver(callback, {
         rootMargin: "0px 0px -20% 0px",
-        threshold: 0
+        threshold: 0,
     });
-    var observeItems = function (items) {
-        items.forEach(function (item) {
+    const observeItems = (items) => {
+        items.forEach((item) => {
             item.classList.add("pre-animated");
             animationObserver.observe(item);
         });
     };
     observeItems(items);
 };
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
     // trigger scroll entry animations
     animationDriver();
     // On the home hero, enable the hover effect, bringing one image to the front. delayed so they can view the today photo before being able to switch back and forth
     setTimeout(function () {
-        var element = document.getElementById("home-hero-image");
-        console.log(element);
+        const element = document.getElementById("home-hero-image");
+        // console.log(element);
         if (element) {
-            element.addEventListener("mouseover", function () {
+            element.addEventListener("mouseover", () => {
                 // Add hover shift of z-index
                 element.style.zIndex = "500"; // bring to front
                 // console.log("hovered on");
             });
-            element.addEventListener("mouseout", function () {
+            element.addEventListener("mouseout", () => {
                 // Remove hover styles
                 element.style.zIndex = "0";
                 // console.log("hovered off");
